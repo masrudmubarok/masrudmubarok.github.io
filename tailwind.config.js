132c29695d831a72592ef8a40f4aef591fbf57aa
masrudmubarok <masrud.mubarok21@gmail.com>
2023-05-21 17:36:14 +0700

    Update all resources to v2

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0284c7',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }  
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }  
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
+      }
+    },
+  },
+  plugins: [],
+}
+
