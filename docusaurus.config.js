0cb3a5ac7cdc8f0333ab0feb4b1bd90c6284efda
masrudmubarok <masrud.mubarok21@gmail.com>
2025-02-17 21:36:38 +0700

    Adding Ads

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -161,7 +161,16 @@ const config = {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
-  }),  
+  }),
+    
+  scripts: [
+    {
+      src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+      async: true,
+      crossorigin: "anonymous",
+      "data-ad-client": "ca-pub-3468535792953225",
+    },
+  ],
 };
 
 export default config;
