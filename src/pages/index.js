c11dceb73e8d9ad8b09aecb09e3fae59ba79097c
masrudmubarok <masrud.mubarok21@gmail.com>
2025-02-17 21:13:29 +0700

    Fix responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -84,7 +84,7 @@ export default function Home() {
     <Layout
       title={`Devlite`}
       description="Description will go into a meta tag in <head />">
-      <div className={styles.scrollContainer}>
+      <div className={styles.responsiveContainer}> {/* Ganti dengan responsiveContainer */}
         <HomepageHeader />
         <HomepageAboutWrapper />
         <HomepageRoadmapWrapper />
