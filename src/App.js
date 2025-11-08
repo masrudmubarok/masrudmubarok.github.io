9413a380be163ebe84b66d03f61a45c90418b0ee
masrudmubarok <masrud.mubarok21@gmail.com>
2025-11-08 12:04:17 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 function App() {
   const [theme, setTheme] = useState('light');
